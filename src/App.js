e583bcc6afbd2df8b4f1703d521d23810d50e573
Tiger Ji <tiger.j540@gmail.com>
2024-03-14 16:56:34 -0400

    done w draft

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,19 +12,33 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState([]);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
-      ))}
+      <section className="menu">
+        {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
+          // <p>Bakery Item {index}</p> // replace with BakeryItem component
+          <BakeryItem item={item} key={index} index={index} cart={cart} setCart={setCart} />
+        ))}
+      </section>
 
-      <div>
+      <section>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
-      </div>
+          <p>
+            <strong>
+              Total Price: {cart.map((i) => bakeryData[i].price).reduce((x, y) => x + y, 0).toFixed(2)}
+            </strong>
+          </p>
+
+          <ul>
+            {cart.map((i) => (
+              <li> { bakeryData[i].name } </li>
+            ))}
+          </ul>
+      </section>
     </div>
   );
 }
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,29 @@
-// TODO: create a component that displays a single bakery item
+// ITEM:
+// name: string;
+// description: string;
+// price: number;
+// image: string;
+
+export default function BakeryItem({ index, item, cart, setCart }) {
+  const handleClick = () => {
+    if (!cart.includes(index)) {
+      setCart([...cart, index]);
+    } else {
+      setCart(cart.filter((i) => i !== index));
+    }
+  };
+
+  return (
+    <div className="bakeryItem">
+      <p> { item.name } | { item.price } </p>
+      {/* <p> { item.description } </p> */}
+      <img src={item.image} width={250} alt={item.name} />
+
+      <div style={{ marginTop: "8px" }}>
+        <button onClick={handleClick}>
+          {!cart.includes(index) ? "Add to Cart" : "Remove from Cart"}
+        </button>
+      </div>
+    </div>
+  );
+}
